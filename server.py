e22ec3c0e490f2c6d959343cb72d98e1a0faad66
Pavani Reddy Karumuri <123492563+PAVANIREDDY7730@users.noreply.github.com>
2026-02-03 15:04:54 -0500

    Updated server.py

--- server.py
+++ server.py
@@ -34,6 +34,6 @@ def get_weather():
 
 
 if __name__ == '__main__':
-    print("Starting Waitress server on http://127.0.0.1:8000", flush=True)
-    #serve(app, host="0.0.0.0", port=8000)
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    #print("Starting Waitress server on http://127.0.0.1:8000", flush=True)
+    serve(app, host="0.0.0.0", port=8000)
+    #app.run(host='127.0.0.1', port=8000, debug=True)
